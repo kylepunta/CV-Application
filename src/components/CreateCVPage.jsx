89d71e38368b3f6d479833c2e1e932e4194e04d8
kylepunta <kylepurcell1998@gmail.com>
2025-02-11 13:23:42 +0000

    Created a react component called CreateCVPage which renders a form for entering CV details

---
+++ src/components/CreateCVPage.jsx
@@ -0,0 +1,11 @@
+import "../styles/CreateCVPage.css";
+import Form from "./Form.jsx";
+
+function CreateCVPage() {
+  return (
+    <main className="create-cv-page">
+      <Form />
+    </main>
+  );
+}
+export default CreateCVPage;
