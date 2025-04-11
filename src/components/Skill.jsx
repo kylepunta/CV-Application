8ef08e1a26de1cc35bbeb79a2406286a505bec66
kylepunta <kylepurcell1998@gmail.com>
2025-04-11 09:36:01 +0100

    Removed buttons section and use states from Skill. Replaced return component with a p element and svg

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,46 +1,18 @@
-import { useState } from "react";
-import { ConfirmButton, EditButton, DeleteButton } from "./Button";
-
 function Skill(props) {
-  const [confirmItem, setConfirmItem] = useState(false);
-  const [editItem, setEditItem] = useState(true);
-  const [disableItem, setDisableItem] = useState(false);
-
   return (
     <>
-      <div className="skill-item">
-        <div className={confirmItem === true && "disabled"}>
-          <p>
-            <label htmlFor="skill">Skill:</label>
-            <input type="text" id="skill" />
-          </p>
-        </div>
-        <div className="buttons-section">
-          {confirmItem === true && (
-            <>
-              <EditButton
-                setConfirmItem={setConfirmItem}
-                setEditItem={setEditItem}
-                setDisableItem={setDisableItem}
-              />
-            </>
-          )}
-          {confirmItem === false && (
-            <>
-              <ConfirmButton
-                setConfirmItem={setConfirmItem}
-                setEditItem={setEditItem}
-                setDisableItem={setDisableItem}
-              />
-              <DeleteButton
-                key={props.id}
-                id={props.id}
-                type={props.type}
-                deleteItem={props.deleteItem}
-                decrementItemCounter={props.decrementItemCounter}
-              />
-            </>
-          )}
+      <div className="skill" id={props.id}>
+        <p>{props.value}</p>
+        <div className="svg-container">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="40px"
+            height="40px"
+          >
+            <title>close</title>
+            <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+          </svg>
         </div>
       </div>
     </>
