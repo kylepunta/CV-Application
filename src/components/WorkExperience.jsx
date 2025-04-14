12c4150e7b878810ce6d5b7bd9a2c6582d8d88e9
kylepunta <kylepurcell1998@gmail.com>
2025-04-14 12:56:01 +0100

    Created end date useRef to disable the correct element when a user clicks present; added value and onChange properties to inputs

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { ConfirmButton, EditButton, DeleteButton } from "./Button";
 
 function WorkExperience(props) {
+  const endDateRef = useRef(null);
   const [confirmItem, setConfirmItem] = useState(false);
   const [editItem, setEditItem] = useState(true);
   const [disableItem, setDisableItem] = useState(false);
@@ -12,93 +13,217 @@ function WorkExperience(props) {
         <div className={disableItem === true && "disabled"}>
           <p>
             <label htmlFor="employerName">Employer Name:</label>
-            <input type="text" id="employerName" />
+            <input
+              type="text"
+              id="employerName"
+              value={props.work.employerName}
+              onChange={(e) => {
+                props.setWork((previousWork) => {
+                  return previousWork.map((work) => {
+                    if (work.id === props.id) {
+                      return { ...work, employerName: e.target.value };
+                    } else {
+                      return { ...work };
+                    }
+                  });
+                });
+              }}
+            />
           </p>
           <p>
             <label htmlFor="position">Position Held:</label>
-            <input type="text" id="position" />
+            <input
+              type="text"
+              id="position"
+              value={props.work.position}
+              onChange={(e) => {
+                props.setWork((previousWork) => {
+                  return previousWork.map((work) => {
+                    if (work.id === props.id) {
+                      return { ...work, position: e.target.value };
+                    } else {
+                      return { ...work };
+                    }
+                  });
+                });
+              }}
+            />
           </p>
           <p className="dropdown-section">
             <label htmlFor="startDate">Start Date:</label>
             <div className="start-date-dropdowns">
               <div>
-                <button className="month-dropdown">
-                  <span>Month</span>
-                  <div className="dropdown-arrow">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <title>chevron-down</title>
-                      <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                    </svg>
-                  </div>
-                </button>
-                <ul className="months hidden">
-                  <li>January</li>
-                  <li>February</li>
-                  <li>March</li>
-                  <li>April</li>
-                  <li>May</li>
-                  <li>June</li>
-                  <li>July</li>
-                  <li>August</li>
-                  <li>September</li>
-                  <li>August</li>
-                  <li>November</li>
-                  <li>December</li>
-                </ul>
+                <select
+                  className="month"
+                  value={props.work.startDateMonth}
+                  onChange={(e) => {
+                    props.setWork((previousWork) => {
+                      return previousWork.map((work) => {
+                        if (work.id === props.id) {
+                          return { ...work, startDateMonth: e.target.value };
+                        } else {
+                          return { ...work };
+                        }
+                      });
+                    });
+                  }}
+                >
+                  <option value="">Month</option>
+                  <option value="January">January</option>
+                  <option value="February">February</option>
+                  <option value="March">March</option>
+                  <option value="April">April</option>
+                  <option value="May">May</option>
+                  <option value="June">June</option>
+                  <option value="July">July</option>
+                  <option value="August">August</option>
+                  <option value="September">September</option>
+                  <option value="October">October</option>
+                  <option value="November">November</option>
+                  <option value="December">December</option>
+                </select>
               </div>
-              <button className="year-dropdown">
-                <span>Year</span>
-                <div className="dropdown-arrow">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <title>chevron-down</title>
-                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                  </svg>
-                </div>
-              </button>
+              <select
+                className="year"
+                value={props.work.startDateYear}
+                onChange={(e) => {
+                  props.setWork((previousWork) => {
+                    return previousWork.map((work) => {
+                      if (work.id === props.id) {
+                        return { ...work, startDateYear: e.target.value };
+                      } else {
+                        return { ...work };
+                      }
+                    });
+                  });
+                }}
+              >
+                <option value="">Year</option>
+                {Array.from({ length: 50 }, (_, i) => {
+                  const year = 1980 + i;
+                  return (
+                    <option key={year} value={year}>
+                      {year}
+                    </option>
+                  );
+                })}
+              </select>
             </div>
           </p>
           <p className="dropdown-section">
             <label htmlFor="endDate">End Date:</label>
-            <div className="end-date-dropdowns">
+            <div className="end-date-dropdowns" ref={endDateRef}>
               <div>
-                <button className="month-dropdown">
-                  <span>Month</span>
-                  <div className="dropdown-arrow">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <title>chevron-down</title>
-                      <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                    </svg>
-                  </div>
-                </button>
-                <ul className="months hidden">
-                  <li>January</li>
-                  <li>February</li>
-                  <li>March</li>
-                  <li>April</li>
-                  <li>May</li>
-                  <li>June</li>
-                  <li>July</li>
-                  <li>August</li>
-                  <li>September</li>
-                  <li>August</li>
-                  <li>November</li>
-                  <li>December</li>
-                </ul>
+                <select
+                  className="month"
+                  value={props.work.endDateMonth}
+                  onChange={(e) => {
+                    props.setWork((previousWork) => {
+                      return previousWork.map((work) => {
+                        if (work.id === props.id) {
+                          return { ...work, endDateMonth: e.target.value };
+                        } else {
+                          return { ...work };
+                        }
+                      });
+                    });
+                  }}
+                >
+                  <option value="">Month</option>
+                  <option value="January">January</option>
+                  <option value="February">February</option>
+                  <option value="March">March</option>
+                  <option value="April">April</option>
+                  <option value="May">May</option>
+                  <option value="June">June</option>
+                  <option value="July">July</option>
+                  <option value="August">August</option>
+                  <option value="September">September</option>
+                  <option value="October">October</option>
+                  <option value="November">November</option>
+                  <option value="December">December</option>
+                </select>
               </div>
-              <button className="year-dropdown">
-                <span>Year</span>
-                <div className="dropdown-arrow">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <title>chevron-down</title>
-                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                  </svg>
-                </div>
-              </button>
+              <select
+                className="year"
+                value={props.work.endDateYear}
+                onChange={(e) => {
+                  props.setWork((previousWork) => {
+                    return previousWork.map((work) => {
+                      if (work.id === props.id) {
+                        return { ...work, endDateYear: e.target.value };
+                      } else {
+                        return { ...work };
+                      }
+                    });
+                  });
+                }}
+              >
+                <option value="">Year</option>
+                {Array.from({ length: 50 }, (_, i) => {
+                  const year = 1980 + i;
+                  return (
+                    <option key={year} value={year}>
+                      {year}
+                    </option>
+                  );
+                })}
+              </select>
             </div>
+            <p className="present-field">
+              <input
+                className="checkbox"
+                type="checkbox"
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    endDateRef.current.classList.add("hidden");
+                    props.setEducation((prev) => {
+                      const newEducation = [...prev];
+                      for (let i = 0; i < newEducation.length; i++) {
+                        if (newEducation[i].id === props.education.id) {
+                          newEducation[i].endDate.month = "present";
+                          newEducation[i].endDate.year = "present";
+                        }
+                      }
+                      return newEducation;
+                    });
+                  } else {
+                    endDateRef.current.classList.remove("hidden");
+                    props.setEducation((prev) => {
+                      const newEducation = [...prev];
+                      for (let i = 0; i < newEducation.length; i++) {
+                        if (newEducation[i].id === props.education.id) {
+                          newEducation[i].endDate.month = "";
+                          newEducation[i].endDate.year = "";
+                        }
+                      }
+                      return newEducation;
+                    });
+                  }
+                }}
+              />
+              <label className="present" htmlFor="present">
+                Present
+              </label>
+            </p>
           </p>
           <p className="description-section">
             <label htmlFor="responsibilites">Main Responsibilities:</label>
-            <textarea id="responsibilities"></textarea>
+            <textarea
+              id="responsibilities"
+              value={props.work.responsibilities}
+              onChange={(e) => {
+                props.setWork((previousWork) => {
+                  return previousWork.map((work) => {
+                    if (work.id === props.id) {
+                      return { ...work, responsibilities: e.target.value };
+                    } else {
+                      return { ...work };
+                    }
+                  });
+                });
+              }}
+            ></textarea>
           </p>
         </div>
         <div className="buttons-section">
@@ -121,9 +246,9 @@ function WorkExperience(props) {
               <DeleteButton
                 key={props.id}
                 id={props.id}
+                setWork={props.setWork}
                 type={props.type}
                 deleteItem={props.deleteItem}
-                decrementItemCounter={props.decrementItemCounter}
               />
             </>
           )}
