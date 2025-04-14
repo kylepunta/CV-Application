158c2da975884b43dd079f2bcf13d5f702de6e8e
kylepunta <kylepurcell1998@gmail.com>
2025-04-14 12:54:29 +0100

    Removed unused code; added value and onChange properties to textarea element

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,14 @@
-import { useState } from "react";
-import { ConfirmButton, EditButton } from "./Button";
-
-function Profile() {
-  const [confirmItem, setConfirmItem] = useState(false);
-  const [editItem, setEditItem] = useState(true);
-  const [disableItem, setDisableItem] = useState(false);
-
+function Profile(props) {
   return (
     <>
-      <p
-        className={
-          confirmItem === true
-            ? "description-section disabled"
-            : "description-section"
-        }
-      >
-        <textarea placeholder="Make a great impression by presenting yourself in a few sentences"></textarea>
+      <p className="description-section">
+        <textarea
+          placeholder="Make a great impression by presenting yourself in a few sentences"
+          value={props.profile}
+          onChange={(e) => {
+            props.setProfile(e.target.value);
+          }}
+        ></textarea>
       </p>
     </>
   );
