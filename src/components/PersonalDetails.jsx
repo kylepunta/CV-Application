9db7be11430ecddd2a23e06b382a001cd071bb57
kylepunta <kylepurcell1998@gmail.com>
2025-04-14 12:54:07 +0100

    Removed unused code; added value and onChange properties to inputs

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,47 +1,136 @@
-import { useState } from "react";
-import { ConfirmButton, EditButton } from "./Button";
-
-function PersonalDetails() {
-  const [confirmItem, setConfirmItem] = useState(false);
-  const [editItem, setEditItem] = useState(true);
-  const [disableItem, setDisableItem] = useState(false);
-
+function PersonalDetails(props) {
   return (
     <>
       <div className="personal-details">
-        <div className={confirmItem === true && "disabled"}>
+        <div>
           <p>
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" />
+            <input
+              type="text"
+              id="name"
+              value={props.personalDetails.name}
+              onChange={(e) =>
+                props.setPersonalDetails((previousDetails) => {
+                  const newDetails = { ...previousDetails };
+                  newDetails.name = e.target.value;
+                  return newDetails;
+                })
+              }
+            />
           </p>
           <p>
             <label htmlFor="dateOfBirth">Date Of Birth:</label>
-            <input type="date" id="dateOfBirth" />
+            <input
+              type="date"
+              id="dateOfBirth"
+              value={props.personalDetails.dateOfBirth}
+              onChange={(e) =>
+                props.setPersonalDetails((previousDetails) => {
+                  const newDetails = { ...previousDetails };
+                  newDetails.dateOfBirth = e.target.value;
+                  return newDetails;
+                })
+              }
+            />
           </p>
           <legend>Address</legend>
           <p>
             <label htmlFor="addressLineOne">Address Line 1:</label>
-            <input type="text" id="addressLineOne" />
+            <input
+              type="text"
+              id="addressLineOne"
+              value={props.personalDetails.address[0]}
+              onChange={(e) =>
+                props.setPersonalDetails((previousDetails) => {
+                  const newDetails = { ...previousDetails };
+                  if (Array.isArray(newDetails.address)) {
+                    newDetails.address[0] = e.target.value;
+                  }
+                  return newDetails;
+                })
+              }
+            />
           </p>
           <p>
             <label htmlFor="addressLineTwo">Address Line 2:</label>
-            <input type="text" id="addressLineTwo" />
+            <input
+              type="text"
+              id="addressLineTwo"
+              value={props.personalDetails.address[1]}
+              onChange={(e) =>
+                props.setPersonalDetails((previousDetails) => {
+                  const newDetails = { ...previousDetails };
+                  if (Array.isArray(newDetails.address)) {
+                    newDetails.address[1] = e.target.value;
+                  }
+                  return newDetails;
+                })
+              }
+            />
           </p>
           <p>
             <label htmlFor="addressCity">City:</label>
-            <input type="text" id="addressCity" />
+            <input
+              type="text"
+              id="addressCity"
+              value={props.personalDetails.address[2]}
+              onChange={(e) =>
+                props.setPersonalDetails((previousDetails) => {
+                  const newDetails = { ...previousDetails };
+                  if (Array.isArray(newDetails.address)) {
+                    newDetails.address[2] = e.target.value;
+                  }
+                  return newDetails;
+                })
+              }
+            />
           </p>
           <p>
             <label htmlFor="addressCountry">Country:</label>
-            <input type="text" id="addressCountry" />
+            <input
+              type="text"
+              id="addressCountry"
+              value={props.personalDetails.address[3]}
+              onChange={(e) =>
+                props.setPersonalDetails((previousDetails) => {
+                  const newDetails = { ...previousDetails };
+                  if (Array.isArray(newDetails.address)) {
+                    newDetails.address[3] = e.target.value;
+                  }
+                  return newDetails;
+                })
+              }
+            />
           </p>
           <p>
             <label htmlFor="email">Email Address:</label>
-            <input type="email" id="email" />
+            <input
+              type="email"
+              id="email"
+              value={props.personalDetails.email}
+              onChange={(e) =>
+                props.setPersonalDetails((previousDetails) => {
+                  const newDetails = { ...previousDetails };
+                  newDetails.email = e.target.value;
+                  return newDetails;
+                })
+              }
+            />
           </p>
           <p>
             <label htmlFor="phoneNumber">Phone Number:</label>
-            <input type="tel" id="phoneNumber" />
+            <input
+              type="tel"
+              id="phoneNumber"
+              value={props.personalDetails.phoneNumber}
+              onChange={(e) =>
+                props.setPersonalDetails((previousDetails) => {
+                  const newDetails = { ...previousDetails };
+                  newDetails.phone = e.target.value;
+                  return newDetails;
+                })
+              }
+            />
           </p>
         </div>
       </div>
