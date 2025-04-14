dcc236a982eee1ecde7333c4957ed2ee520110b3
kylepunta <kylepurcell1998@gmail.com>
2025-04-14 12:53:46 +0100

    Removed unused code; added value and onChange properties to textarea element

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,21 +1,14 @@
-import { useState } from "react";
-import { ConfirmButton, EditButton, DeleteButton } from "./Button";
-
 function Interests(props) {
-  const [confirmItem, setConfirmItem] = useState(false);
-  const [editItem, setEditItem] = useState(true);
-  const [disableItem, setDisableItem] = useState(false);
-
   return (
     <>
-      <p
-        className={
-          confirmItem === true
-            ? "description-section disabled"
-            : "description-section"
-        }
-      >
-        <textarea placeholder="A brief summary of your interests"></textarea>
+      <p className="description-section">
+        <textarea
+          placeholder="A brief summary of your interests"
+          value={props.interests}
+          onChange={(e) => {
+            props.setInterests(e.target.value);
+          }}
+        ></textarea>
       </p>
     </>
   );
